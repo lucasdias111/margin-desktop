5f5ac7ccdd62ecf3801de79372418902b222c80f
Lucas Dias <lucasdias0@proton.me>
2025-12-07 16:27:38 +0100

    remove unused css

--- src/lib/components/sidebar/Sidebar.svelte
+++ src/lib/components/sidebar/Sidebar.svelte
@@ -20,37 +20,4 @@
     padding: 20px;
     overflow-y: auto;
   }
-
-  .sidebar h3 {
-    margin: 0 0 15px 0;
-  }
-
-  .sidebar ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  .sidebar button {
-    width: 100%;
-    padding: 10px;
-    background-color: transparent;
-    border: none;
-    color: inherit;
-    text-align: left;
-    cursor: pointer;
-    font: inherit;
-  }
-
-  .sidebar li {
-    padding: 10px;
-    margin-bottom: 5px;
-    background-color: #333;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-
-  .sidebar li:hover {
-    background-color: #3a3a3a;
-  }
 </style>
